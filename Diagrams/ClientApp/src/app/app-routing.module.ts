514d17041210366ad6cd91e66f6dfbc8773e5aeb
Ян <gorshkov.yan1998@gmail.com>
2019-03-24 21:08:17 +0200

    Changed routing modules -  fixed home route and added default route

--- Diagrams/ClientApp/src/app/app-routing.module.ts
+++ Diagrams/ClientApp/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PieChartComponent } from './pie-chart/pie-chart.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo: ''},
-  {path: 'pie-chart', component: PieChartComponent}
+  {path: '', pathMatch: 'full', redirectTo: '/home'},
+  {path: 'home', component: HomeComponent},
+  {path: 'pie-chart', component: PieChartComponent},
+  {path: '**', redirectTo: '/home'}
 ];
 
 @NgModule({
