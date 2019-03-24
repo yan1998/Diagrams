953ef4c79184927570b0a9d0ae12b5cc2569e662
Ян <gorshkov.yan1998@gmail.com>
2019-03-24 18:33:45 +0200

    Deleted default components from project and change package.json file (fixes#1)

--- Diagrams/ClientApp/src/app/app.module.ts
+++ Diagrams/ClientApp/src/app/app.module.ts
@@ -6,17 +6,13 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,9 +20,7 @@ import { ChartsModule } from 'ng2-charts';
     FormsModule,
     ChartsModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '', component: HomeComponent, pathMatch: 'full' }
     ])
   ],
   providers: [],
